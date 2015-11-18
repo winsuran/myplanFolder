69767d3482f8f00a28bc5b4f8e359547a2ecfa22
Suranga Thilakasiri <suranga@suranga-desktop.(none)>
2015-11-18 20:01:02 +0530

    good

---
+++ Students Rev.s
@@ -0,0 +1,98 @@
+Students Revision System
+
+Working properly
+
+Sign up...
+Validation
+Save user details
+
+Login...
+
+Term...
+Add Term details..
+Validation
+Event calculation
+Daily schedule
+
+Edit Term..
+Edit Term details
+
+Add Subject...
+Validation
+Add subjects details
+Add Topics details
+
+Edit Subjects...
+Validation
+Show subjects remaining time
+
+Dashboard...
+Select term 
+Update subject progress
+
+
+
+
+
+
+
+Not working
+
+Sign up...
+Email validaton
+
+
+
+Term...
+Add Term details..
+In event calculation If enter same name to weekend and weekdays, not working.
+
+Edit Term..
+Delete Term
+When edit start date and end date show current date by calender
+
+Edit Subjects...
+Not delete subject
+Not Delete Topics
+
+Some time occured..
+Not edit subjects
+Not edit Topics
+Undefine error message
+Not save edit details in DB
+
+Dashboard...
+Validation(Spend minutes and new Topics)
+Not save new add Topics
+
+Menu bar...
+Not selected icon when choose menu item
+
+UI...
+Dashboard select Term UI is not finished.
+When selec back ground disappear propup UI.
+
+Profile
+profile pic not save in folder
+Newletter
+Delete function
+
+Special
+Delete function
+All login shows all Terms details
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tool
@@ -0,0 +1,43 @@
+IP- 192.168.101.179 
+
+WTS
+host=192.168.101.81                    //localhost 
+username=root
+password=root
+
+host=192.168.101.55                   //localhost 
+username=root
+password=root123
+
+<---------Maven Archetype------>
+ https://maven-repository.com/artifact/org.fluttercode.knappsack/spring-mvc-jpa-demo-archetype/1.1   
+
+
+
+
+
+git
+
+git clone location
+
+git pull
+git status
+git add .
+git commit -m ""
+git push
+
+SVN Path
+http://192.168.101.62/svn/myplan 
+
+
+Life Story of Facebook Boss Mark Zuckerberg - Documentary
+https://www.youtube.com/watch?v=-oWg4XM5Fyw
+
+https://www.youtube.com/watch?v=KxEtofitkuE
+
+https://www.youtube.com/watch?v=fO2u-uxVBIc
+
+https://www.youtube.com/watch?v=7vPJT5KZLtM
+
+Delete
+http://stackoverflow.com/questions/18317424/jqueryspring-remove-from-database
